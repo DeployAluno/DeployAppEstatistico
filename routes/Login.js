db6f9c1915fbde25021ee5e9ea302aa87414d2fe
Luciano Sabino <lb255043@gmail.com>
2025-07-10 23:50:32 -0300

    .

---
+++ routes/Login.js
@@ -0,0 +1,17 @@
+//Importações
+const express           = require('express');
+const router            = express.Router();
+
+const loginController   = require("../controllers/login/Login");
+const recoverPasswordController = require("../controllers/emailBuilder/recoverPassword")
+const authenticate = require("../middleware/authorization");
+//Fim das Importações
+
+//Rotas de Login
+router.post("/login", loginController.login); //Fazer o Login no APP
+//router.post("/login/forgotpassword", recoverPasswordController.recoverPassword) //Metodo para Receber o Email, e envia uma Email de Redefinição
+//router.post("/login/changepassword", authenticate.auth, recoverPasswordController.changingPassword) //Recebe a nova senha e altera
+//Fim Rotas de Login
+
+//Exportando a rota
+module.exports = router;
